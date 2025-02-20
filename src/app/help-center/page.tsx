a6215956027dc158f8a6fd0c4103bd8a923cdf33
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2025-02-21 01:31:40 +0530

    updated new feature for adding custom data

--- src/app/help-center/page.tsx
+++ src/app/help-center/page.tsx
@@ -1,18 +1,20 @@
 import HelpCenter from "@/components/help-center/HelpCenter";
 import clsx from "clsx";
-import React from "react";
+import React, { Suspense } from "react";
 
 function page() {
   return (
-    <div
-      className={clsx(
-        "h-dvh overflow-y-auto relative",
-        "sm:[&::-webkit-scrollbar]:w-1",
-        " sm:[&::-webkit-scrollbar-track]:bg-gray-100/10 sm:[&::-webkit-scrollbar-track]:rounded-xl",
-        " sm:[&::-webkit-scrollbar-thumb]:bg-gray-300/20 sm:[&::-webkit-scrollbar-thumb]:rounded-xl"
-      )}>
-      <HelpCenter />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div
+        className={clsx(
+          "h-dvh overflow-y-auto relative",
+          "sm:[&::-webkit-scrollbar]:w-1",
+          " sm:[&::-webkit-scrollbar-track]:bg-gray-100/10 sm:[&::-webkit-scrollbar-track]:rounded-xl",
+          " sm:[&::-webkit-scrollbar-thumb]:bg-gray-300/20 sm:[&::-webkit-scrollbar-thumb]:rounded-xl"
+        )}>
+        <HelpCenter />
+      </div>
+    </Suspense>
   );
 }
 
